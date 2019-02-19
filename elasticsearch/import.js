301d4a9601f3fb14d7326d0a1c635089d2f6da90
NicolasGn <nicolas.c.guegan@gmail.com>
2019-02-19 09:48:36 +0100

    Elasticsearch import done

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -2,16 +2,62 @@ var elasticsearch = require('elasticsearch');
 var csv = require('csv-parser');
 var fs = require('fs');
 
+const callsIndex = 'calls';
+
 var esClient = new elasticsearch.Client({
   host: 'localhost:9200',
   log: 'error'
 });
 
+esClient.indices.delete(
+  {
+    index: callsIndex
+  },
+  error => {
+    if (error) console.trace(error.message);
+  }
+);
+
+esClient.indices.create(
+  {
+    index: callsIndex
+  },
+  error => {
+    if (error) console.trace(error.message);
+  }
+);
+
+let calls = [];
+
 fs.createReadStream('../911.csv')
-    .pipe(csv())
-    .on('data', data => {
-      // TODO extract one line from CSV
-    })
-    .on('end', () => {
-      // TODO insert data to ES
+  .pipe(csv())
+  .on('data', data => {
+    delete data.e;
+    calls.push({
+      ...data,
+      lat: parseFloat(data.lat),
+      lng: parseFloat(data.lng),
+      zip: parseInt(data.zip)
     });
+  })
+  .on('end', () => {
+    esClient.bulk(bulkQuery(calls), error => {
+      if (error) console.trace(error.message);
+      else console.log(`Inserted ${calls.length} calls`);
+      esClient.close();
+    });
+  });
+
+bulkQuery = calls => {
+  const body = calls.reduce((acc, call) => {
+    acc.push({
+      index: {
+        _index: callsIndex,
+        _type: 'call'
+      }
+    });
+    acc.push(call);
+    return acc;
+  }, []);
+  return { body };
+};
